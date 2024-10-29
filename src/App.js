1f5ebebb0df10ccb5a4bbe9b196816bd7a72cb20
ypparmar77 <ypparmar6@gmail.com>
2024-10-29 15:37:51 +0530

    router

--- src/App.js
+++ src/App.js
@@ -55,14 +55,6 @@ function App() {
     {/* <ParentComponent/> */}
     {/* <CounterComponent/> */}
     {/* <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
     </BrowserRouter> */}
     {/* <Froms/> */}
     {/* <ParentComponent/> */}
